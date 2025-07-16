ea3e4407fa0233fd6c8ccf6657e512589974f2b4
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2025-07-16 04:16:30 -0700

    add diagnostics

--- .config/nvim/lua/jruhland/lsp.lua
+++ .config/nvim/lua/jruhland/lsp.lua
@@ -1,13 +1,27 @@
 vim.lsp.enable({
-    'gopls',
-    'lua-ls',
-    'ts-ls',
-    'rust-analyzer',
-    'tailwindcss',
+  "gopls",
+  "lua-ls",
+  "ts-ls",
+  "rust-analyzer",
+  "tailwindcss",
+})
+
+vim.lsp.config("*", {
+  capabilities = {
+    textDocument = {
+      semanticTokens = {
+        multilineTokenSupport = true,
+      },
+    },
+  },
+  root_markers = { ".git" },
 })
 
 vim.diagnostic.config({
-    virtual_lines = true,
-    underline = true,
+  virtual_lines = true,
+  underline = true,
 })
 
+vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "<leader>dn", vim.diagnostic.goto_next)
