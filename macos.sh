fd3795e02e7225e4f80d13dc3e651a5737e1141b
Jarrod Ruhland <jarrod.ruhland@gmail.com>
2026-01-25 07:56:07 -0800

    remove debugging

--- macos.sh
+++ macos.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -x  # Enable debug mode to see which commands are failing
-
 # Close any open System Settings panes, to prevent them from overriding settings
 osascript -e 'tell application "System Settings" to quit' 2>/dev/null || true
 
